b239bad3969160b051d175aa13f86ce4631e47b4
Yris Siqueira <yris.siqueira@kobe.io>
2019-07-23 10:02:04 -0300

    Adding project

---
+++ VeganOptionsPOA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VeganOptionsPOA
+//
+//  Created by Yris Siqueira on 23/07/19.
+//  Copyright © 2019 Yris Siqueira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
