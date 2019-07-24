0682ef9c10bebc316598b21e1c7b5c9b417cac44
Yris Siqueira <yris.siqueira@kobe.io>
2019-07-23 22:35:36 -0300

    Fixing user model

--- VeganOptionsPOA/Model/User.swift
+++ VeganOptionsPOA/Model/User.swift
@@ -8,7 +8,22 @@
 
 import Foundation
 
-struct User: Codable {
-    var id: String
-    var favoritePlaces: [String]
+class User: Codable {
+    
+    let identifier: String
+    let favoritePlaces: [String]
+    
+    required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: UserCodingKeys.self)
+        identifier = try values.decode(String.self, forKey: .identifier)
+        favoritePlaces = try values.decode([String].self, forKey: .favoritePlaces)
+    }
+    
+}
+
+extension User {
+    enum UserCodingKeys: String, CodingKey {
+        case identifier = "id"
+        case favoritePlaces = "favoritePlaces"
+    }
 }
