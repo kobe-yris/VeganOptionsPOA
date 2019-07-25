8fcc8bff6fbeb181f95731e1245fb12549969627
Camila Prestes <camila.prestes@yahoo.com.br>
2019-07-25 10:12:45 -0300

    Fix information in post tableview

---
+++ VeganOptionsPOA/Scenes/PostsDetail/PostDetailTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PostDetailTableViewCell.swift
+//  VeganOptionsPOA
+//
+//  Created by Camila Legramante Prestes on 25/07/19.
+//  Copyright © 2019 Yris Siqueira. All rights reserved.
+//
+
+import UIKit
+
+class PostDetailTableViewCell: UITableViewCell {
+    @IBOutlet weak var postTitle: UILabel!    
+    @IBOutlet weak var postText: UITextView!
+    @IBOutlet weak var postData: UILabel!
+    
+}
